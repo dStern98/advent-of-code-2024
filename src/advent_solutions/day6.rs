eebb7a52bb2676110cca0090a403f1ecf6d12e04
David Stern <david@mountainvector.com>
2024-12-05 22:32:21 -0800

    finished day6 part2

--- src/advent_solutions/day6.rs
+++ src/advent_solutions/day6.rs
@@ -7,15 +7,20 @@ pub struct  Day6;
 
 #[derive(Debug, Clone)]
 struct Guard {
+    ///Guards current row position
     row: i64,
+    ///Guards current column position
     col: i64,
+    ///Guards current direction
     direction: Direction,
-    visit_history: HashSet<(i64, i64)>
 }
 
 impl Guard {
     fn patrol_lab(&mut self, lab_map: &[Vec<char>]) -> anyhow::Result<()> {
-        self.visit_history.insert((self.row, self.col));
+        //! Move the guard by 1 step. 
+        //! If possible the guard takes 1 step in the same direction they are facing.
+        //! Otherwise, rotate 90 degrees until motion is possible. 
+        //! Returns an error if the guard walks off the lab map (which is actually the goal)
         loop {
             let (next_row, next_col) = match self.direction {
                 Direction::Right => ( self.row, self.col + 1),
@@ -39,6 +44,7 @@ impl Guard {
         }
     }
     fn try_new(lab_map: &[Vec<char>]) -> anyhow::Result<Self> {
+        //! Construct a starting guard from the input lab map.
         for (row_number, row) in lab_map.iter().enumerate() {
             for (col_number, col) in row.iter().enumerate() {
                 let row_number = row_number as i64;
@@ -49,17 +55,25 @@ impl Guard {
                             row: row_number, 
                             col: col_number, 
                             direction: Direction::Up,
-                            visit_history: HashSet::new()
                         })
                     }, 
                     '>' => {
-                        return Ok(Guard { row: row_number, col: col_number, direction: Direction::Right, visit_history: HashSet::new() })
+                        return Ok(Guard { 
+                            row: row_number, 
+                            col: col_number, 
+                            direction: Direction::Right})
                     },
                     'v' => {
-                        return Ok(Guard{row: row_number, col: col_number, direction: Direction::Down, visit_history: HashSet::new()})
+                        return Ok(Guard{
+                            row: row_number, 
+                            col: col_number, 
+                            direction: Direction::Down})
                     },
                     '<' => {
-                        return Ok(Guard { row: row_number, col: col_number, direction: Direction::Left, visit_history: HashSet::new() })
+                        return Ok(Guard { 
+                            row: row_number, 
+                            col: col_number, 
+                            direction: Direction::Left })
                     }
                     _ => {}
                 }
@@ -69,7 +83,7 @@ impl Guard {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 enum Direction {
     Up, 
     Down, 
@@ -90,15 +104,57 @@ impl Direction {
 
 impl SolveAdvent for Day6 {
     fn solve_part1(path_to_file: &str) -> anyhow::Result<()> {
+        //! Let the guard patrol the lab until they walk off the map.
+        //! Count the unique spaces traversed.
         let file_contents = read_input_file(path_to_file)?;
         let lab_map = file_contents.lines().map(|row| row.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
         let mut guard = Guard::try_new(&lab_map)?;
-        while let Ok(_) = guard.patrol_lab(&lab_map) {}
-        println!("Guard visited {:?} unique positions in the lab", guard.visit_history.len());
+        let mut visit_history = HashSet::new();
+        visit_history.insert((guard.row, guard.col));
+        //When the guard patrol_lab call returns an error, the guard
+        // has walked off the map.
+        while guard.patrol_lab(&lab_map).is_ok() {
+            //Keep track of unique visits
+            visit_history.insert((guard.row, guard.col));
+        }
+        println!("Guard visited {:?} unique positions in the lab", visit_history.len());
         Ok(())
     }
 
-    fn solve_part2(_path_to_file: &str) -> anyhow::Result<()> {
+    fn solve_part2(path_to_file: &str) -> anyhow::Result<()> {
+        //! A brute-force simulation to find all single obstacles that can
+        //! be placed to induce the guard to get stuck in an infinite loop. Not sure
+        //! if there is a better way to do this.
+        let file_contents = read_input_file(path_to_file)?;
+        let mut lab_map = file_contents.lines().map(|row| row.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
+        let guard = Guard::try_new(&lab_map)?;
+        let mut successful_guard_loops = 0;
+        for row_number in 0..lab_map.len() {
+            for col_number in 0..lab_map[0].len() {
+                if lab_map[row_number][col_number] == '#' || lab_map[row_number][col_number] == '^' {
+                    //We are not allowed to place a new obstacle if there already is an obstacle
+                    //or this is the guards starting position
+                    continue;
+                }
+                //Temporarily change row,col to contain an obstacle.
+                lab_map[row_number][col_number] = '#';
+                let mut guard = guard.clone();
+                //An infinite loop occurs if the same row, col and direction are ever repeated.
+                //Because the guards progression are deterministic, this gurantees a infinite loop has been reached
+                let mut guard_loop_detector = HashSet::new();
+                guard_loop_detector.insert((guard.row, guard.col, guard.direction));
+                while guard.patrol_lab(&lab_map).is_ok() {
+                    if guard_loop_detector.contains(&(guard.row, guard.col, guard.direction)) {
+                        successful_guard_loops += 1;
+                        break;
+                    }
+                    guard_loop_detector.insert((guard.row, guard.col, guard.direction));
+                }
+                //After each simulation we put the current spot back to being empty. 
+                lab_map[row_number][col_number] = '.';
+            }
+        }
+        println!("There were {} unique obstacles that generate infinite guard loops", successful_guard_loops);
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use crate::advent_solutions::SolveAdvent;
 
 fn main() -> anyhow::Result<()> {
     Day6::solve_part1("input.txt")?;
-    Day6::solve_part2("test.txt")?;
+    Day6::solve_part2("input.txt")?;
     Ok(())
 }
