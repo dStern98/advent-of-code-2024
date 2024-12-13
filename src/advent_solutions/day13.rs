1b84309c07f88759075d2b345cef65e892722bff
David Stern <david@mountainvector.com>
2024-12-12 23:52:44 -0800

    finished Day13 Part1

--- src/advent_solutions/day13.rs
+++ src/advent_solutions/day13.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 
 use anyhow::anyhow;
 
@@ -6,7 +5,7 @@ use super::{read_input_file, SolveAdvent};
 
 pub struct Day13;
 
-type OrderedPair = (i64, i64);
+type OrderedPair = (f64, f64);
 
 #[derive(Debug, Clone)]
 struct ClawMachine {
@@ -16,49 +15,40 @@ struct ClawMachine {
 }
 
 impl ClawMachine {
-    fn try_win_prize_using_algebra(&self) {
-        let (mut a_delta_x, mut a_delta_y) = (f64::from(self.button_a.0 as i32), f64::from(self.button_a.1 as i32));
-        let (mut b_delta_x, b_delta_y) = (f64::from(self.button_b.0 as i32), f64::from(self.button_b.1 as i32));
-        let (mut target_x, mut target_y) = (f64::from(self.prize_position.0 as i32), f64::from(self.prize_position.1 as i32));
-        // Equation 1 is 
-        a_delta_x *= b_delta_y;
-        b_delta_x *= b_delta_y;
-        target_x *= b_delta_y;
+    fn try_win_prize_using_algebra(&self) -> Option<i64> {
+        //!Use Algebra to solve the problem
+        //! It turns out there the is exactly one solution to any given Claw Machine 
+        //! problem, so there is actually nothing to optimize!
+        //! Suppose that we are using example 3 from the problem: 
+        //! 
+        //! Button A: X+17, Y+86
+        //! Button B: X+84, Y+37
+        //! Prize: X=7870, Y=6450
+        let (a_delta_x, a_delta_y) = (self.button_a.0, self.button_a.1);
+        let (b_delta_x, b_delta_y) = (self.button_b.0, self.button_b.1);
+        let (target_x, target_y) = (self.prize_position.0, self.prize_position.1);
+        // Equation 1 is The x's equation: 17A + 84B = 7870
+        let scaled_a_dx = a_delta_x * b_delta_y;
+        let scaled_target_x = target_x * b_delta_y;
+        //Equation 2 is the Y's equation: 86A + 37B = 6450
+        let scaled_a_dy = a_delta_y * b_delta_x;
+        let scaled_target_y =   target_y * b_delta_x;
 
-        a_delta_y *= b_delta_x;
-        // b_delta_y *= b_delta_x;
-        target_y *= b_delta_x;
-
-        let a_presses = (target_x - target_y) / (a_delta_x - a_delta_y);
-        let b_presses = (target_x - a_delta_x * a_presses) / b_delta_x;
-        println!("A, B = {},{} presses", a_presses, b_presses);
-        
-    }
-    fn try_win_prize_recursively(&self, current_position: OrderedPair, depth: usize, memo: &mut HashMap<OrderedPair, Option<usize>>) -> Option<usize> {
-        if let Some(cache_item) = memo.get(&current_position) {
-            return *cache_item
-        }
-        if depth > 200 || current_position.0 > self.prize_position.0 || current_position.1 > self.prize_position.1 {
-            //Return as max depth has been reached
-            memo.insert(current_position, None);
+        let a_presses = (scaled_target_x - scaled_target_y) / (scaled_a_dx - scaled_a_dy);
+        let b_presses = (target_x - (a_presses * a_delta_x)) / b_delta_x;
+        if !a_presses.is_finite() || !b_presses.is_finite() || a_presses.is_sign_negative() || b_presses.is_sign_negative() {
             return None;
         }
-        if current_position == self.prize_position {
-            return Some(0);
+
+        if a_presses % 1.0 == 0.0 && b_presses % 1.0 == 0.0 {
+            let a_presses = a_presses.trunc() as i64;
+            let b_presses = b_presses.trunc() as i64;
+            return Some(a_presses * 3 + b_presses);
         }
-        let response_button_a = self.try_win_prize_recursively((current_position.0 + self.button_a.0, current_position.1 + self.button_a.1), depth + 1, memo);
-        let response_button_b = self.try_win_prize_recursively((current_position.0 + self.button_b.0, current_position.1 + self.button_b.1), depth + 1, memo);
-        if response_button_a.is_none() && response_button_b.is_none() {
-            memo.insert(current_position, None);
-            return None;
-        }
-        let response_button_a = response_button_a.unwrap_or(usize::MAX - 3) + 3; // It costs 3 tokens press button A
-        let response_button_b = response_button_b.unwrap_or(usize::MAX - 1) + 1; // It costs 1 token to press button b
-        let result = Some(response_button_a.min(response_button_b));
-        memo.insert(current_position, result);
-        result
+        None
         
     }
+
     fn parse_prize_into_ordered_pair(line: &str) -> anyhow::Result<OrderedPair> {
         let trimmed_num = line.replace("Prize:", "").replace("X=", "").replace("Y=", "");
         let isolated_nums: Result<Vec<_>, _> = trimmed_num.split(',').filter_map(|item| {
@@ -66,8 +56,8 @@ impl ClawMachine {
                 return Some(item.trim())
             }
             None
-        }).map(|item| item.parse::<i64>()).collect();
-        let [x_plus, y_plus]: [i64; 2] = isolated_nums?.try_into().map_err(|_| anyhow!("Could not coerce into length 2 array"))?;
+        }).map(|item| item.parse::<f64>()).collect();
+        let [x_plus, y_plus]: [f64; 2] = isolated_nums?.try_into().map_err(|_| anyhow!("Could not coerce into length 2 array"))?;
         Ok((x_plus, y_plus))
     }
     fn parse_button_into_ordered_pair(line: &str) -> anyhow::Result<OrderedPair> {
@@ -77,8 +67,8 @@ impl ClawMachine {
                 return Some(item.trim())
             }
             None
-        }).map(|item| item.parse::<i64>()).collect();
-        let [x_plus, y_plus]: [i64; 2] = isolated_nums?.try_into().map_err(|_| anyhow!("Could not coerce into length 2 array"))?;
+        }).map(|item| item.parse::<f64>()).collect();
+        let [x_plus, y_plus]: [f64; 2] = isolated_nums?.try_into().map_err(|_| anyhow!("Could not coerce into length 2 array"))?;
         Ok((x_plus, y_plus))
     
     }
@@ -122,19 +112,33 @@ impl SolveAdvent for Day13 {
     fn solve_part1(path_to_file: &str) -> anyhow::Result<()> {
         let file_contents = read_input_file(path_to_file)?;
         let claw_machines = ClawMachine::construct_claws(&file_contents)?;
-        let mut min_total_tokens = 0;
+        let mut total_tokens_used = 0;
         for claw_machine in claw_machines {
-            let mut memo = HashMap::new();
-            if let Some(minimum_tokens_to_win) = claw_machine.try_win_prize_recursively((0, 0), 0, &mut memo) {
-                min_total_tokens += minimum_tokens_to_win;
+            if let Some(solution) = claw_machine.try_win_prize_using_algebra() {
+                total_tokens_used += solution;
             }
 
         }
-        println!("Minimum total tokens to win is {}", min_total_tokens);
+        println!("Total tokens used is {}", total_tokens_used);
         Ok(())
     }
 
-    fn solve_part2(_path_to_file: &str) -> anyhow::Result<()> {
+    fn solve_part2(path_to_file: &str) -> anyhow::Result<()> {
+        //! In theory the code here should work, but most likely a floating point error is causing issues
+        let file_contents = read_input_file(path_to_file)?;
+        let mut claw_machines = ClawMachine::construct_claws(&file_contents)?;
+        for claw_machine in claw_machines.iter_mut() {
+            claw_machine.prize_position.0 += 10000000000000.0;
+            claw_machine.prize_position.1 += 10000000000000.0;
+        }
+        let mut total_tokens_used = 0;
+        for claw_machine in claw_machines {
+            if let Some(solution) = claw_machine.try_win_prize_using_algebra() {
+                total_tokens_used += solution;
+            }
+
+        }
+        println!("Total tokens used is {}", total_tokens_used);
         Ok(())
     }
 }
