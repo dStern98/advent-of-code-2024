c7c8d52cb403342dc3aeb144c213c5968db9756a
David Stern <davidanah98@gmail.com>
2025-01-14 23:16:18 -0800

    Day23 Part2 Solved

--- src/advent_solutions/day23.rs
+++ src/advent_solutions/day23.rs
@@ -59,7 +59,84 @@ impl SolveAdvent for Day23 {
         Ok(())
     }
 
-    fn solve_part2(_path_to_file: &str) -> anyhow::Result<()> {
+    fn solve_part2(path_to_file: &str) -> anyhow::Result<()> {
+        let file_contents = read_input_file(path_to_file)?;
+        let mut connection_topology = construct_topology(&file_contents)?;
+        for (computer, connected_computers) in connection_topology.iter_mut() {
+            connected_computers.insert(computer.to_string());
+        }
+
+        let mut optimizer = HashSet::new();
+        let mut largest_totally_connected_network = HashSet::new();
+        for starting_computer in connection_topology.keys() {
+            let largest_network = find_largest_connected_network( &mut optimizer, &starting_computer, &connection_topology);
+            if largest_network.len() > largest_totally_connected_network.len() {
+                largest_totally_connected_network = largest_network;
+            }
+        }
+        let mut lan_network_password = largest_totally_connected_network.into_iter().collect::<Vec<_>>();
+        lan_network_password.sort();
+        println!("LAN Password is {:?}", lan_network_password.join(","));
         Ok(())
+    }
+}
+
+
+fn find_largest_connected_network( optimizer: &mut HashSet<(String, String)>, starting_position: &str, network: &ConnectionTopology) -> HashSet<String> {
+    let mut largest_connected_network = HashSet::new();
+    let mut traversal_queue = vec![NetworkTraveler::new(starting_position.to_string(), HashSet::new(), network)];
+    while let Some(mut current_traveler) = traversal_queue.pop() {
+        let mut visited =  current_traveler.visited.clone().into_iter().collect::<Vec<_>>();
+        visited.sort();
+        let optimizer_insert = (visited.join(","),current_traveler.current.clone());
+        if current_traveler.in_cycle() || optimizer.contains(&optimizer_insert)  {
+            continue;    
+        }
+        current_traveler.visit();
+        optimizer.insert(optimizer_insert);
+        let perfectly_connnected = current_traveler.perfectly_connected();
+        if !perfectly_connnected {
+            continue;
+        }
+        if perfectly_connnected && current_traveler.visited.len() > largest_connected_network.len() {
+            largest_connected_network = current_traveler.visited.clone();
+        }
+        traversal_queue.extend(current_traveler.spawn_next());
+
+    }
+    largest_connected_network
+}
+
+struct NetworkTraveler<'a> {
+    visited: HashSet<String>,
+    current: String,
+    network: &'a ConnectionTopology
+}
+
+impl <'a>NetworkTraveler <'a> {
+    fn new(current: String, visited: HashSet<String>, network: &'a ConnectionTopology) -> Self {
+        NetworkTraveler {
+            current, 
+            visited,
+            network
+        }
+    }
+    fn in_cycle(&self) -> bool {
+        self.visited.contains(&self.current)
+    }
+    fn visit(&mut self) {
+        self.visited.insert(self.current.clone());
+    }
+    fn spawn_next(&self) -> Vec<Self> {
+        self.network.get(&self.current).unwrap().iter().map(|next_computer| NetworkTraveler::new(next_computer.clone(),  self.visited.clone(), &self.network )).collect::<Vec<_>>()
+    }
+
+    fn perfectly_connected(&self) -> bool {
+        for computer in self.visited.iter() {
+            if !self.visited.is_subset(self.network.get(computer).unwrap()) {
+                return false;
+            }
+        }
+        true
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod advent_solutions;
-use crate::advent_solutions::day20::Day20;
+use crate::advent_solutions::day23::Day23;
 use crate::advent_solutions::SolveAdvent;
 
 fn main() -> anyhow::Result<()> {
-    Day20::solve_part1("input.txt")?;
-    Day20::solve_part2("input.txt")?;
+    Day23::solve_part1("input.txt")?;
+    Day23::solve_part2("input.txt")?;
     Ok(())
 }
